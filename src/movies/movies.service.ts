753a209b7b029a31c62f49a8e3f234a19530ad4c
peacemaker474 <hc456123@gmail.com>
2023-12-04 16:00:51 +0900

    feat: 업데이트 DTO 및 nestjs/mapped-types 추가

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Injectable()
 export class MoviesService {
@@ -28,7 +29,7 @@ export class MoviesService {
     });
   }
 
-  update(id: number, updateData) {
+  update(id: number, updateData: UpdateMovieDto) {
     const movie = this.getOne(id);
     this.deleteOne(id);
     this.movies.push({ ...movie, ...updateData });
