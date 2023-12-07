c232bbb346c27fa218c43903b265fe5c724c1223
peacemaker474 <hc456123@gmail.com>
2023-12-07 19:28:18 +0900

    chore: Swagger 설치 및 일부 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
@@ -11,6 +13,17 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('Swagger-tutorial')
+    .setDescription('User API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
